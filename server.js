09322e6e51c68414a921816b4e987b89188f9aa3
sovannratana <ouk.sovannratana19@kit.edu.kh>
2022-06-20 12:21:01 +0700

    read single book and single author & doing the add mutation for book and author

--- server.js
+++ server.js
@@ -74,10 +74,65 @@ const RootQueryType = new GraphQLObjectType({
       description: "List of all books",
       resolve: () => books
     },
+    book: {
+      type: BookType,
+      description: "A single book",
+      args: {
+        id: {type: GraphQLInt}
+      },
+      resolve: (parent, args) => books.find(book => book.id === args.id)
+    },
     authors: {
       type: new GraphQLList(AuthorType),
       description: "List of all authors",
       resolve: () => authors
+    },
+    author: {
+      type: AuthorType,
+      description: "A single author",
+      args: {
+        id: {type: GraphQLInt},
+      },
+      resolve: (parent, args) => authors.find(author => author.id === args.id)
+    },
+  })
+})
+
+const RootMutationType = new GraphQLObjectType({
+  name: "Mutation",
+  description: "Root Mutation",
+  fields: () => ({
+    addBook: {
+      type: BookType,
+      description: "Add a book",
+      args: {
+        name: {type: GraphQLNonNull(GraphQLString)},
+        authorId: {type: GraphQLNonNull(GraphQLInt)},
+      },
+      resolve: (parent, args) => {
+        const book = {
+          id: books.length+1,
+          name: args.name,
+          authorId: args.authorId,
+        }
+        books.push(book)
+        return book
+      }
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: "Add an author",
+      args: {
+        name: {type: GraphQLNonNull(GraphQLString)},
+      },
+      resolve: (parent, args) => {
+        const author = {
+          id: authors.length+1,
+          name: args.name,
+        }
+        authors.push(author)
+        return author
+      }
     }
   })
 })
@@ -85,6 +140,7 @@ const RootQueryType = new GraphQLObjectType({
 // schema
 const schema = new GraphQLSchema({
   query: RootQueryType,
+  mutation: RootMutationType,
 })
 
 app.use("/graphql", graphqlHTTP({
